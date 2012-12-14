de20ac8b0ecf33e820d6367db32ba796dc9c0ea6
Vincent W <vwassenaar@hotmail.com>
2012-12-14 18:11:08 +0100

    Added PathFinder.java

---
+++ PowerWalk/src/powerwalk/control/PathFinder.java
@@ -0,0 +1,44 @@
+package powerwalk.control;
+
+import java.util.ArrayList;
+import powerwalk.Bot;
+import powerwalk.model.Collision;
+import powerwalk.model.Point;
+
+/**
+ * This class finds a defined path between 2 points.
+ *
+ * @author Vincent W
+ */
+public class PathFinder {
+
+    public ArrayList<Point> calculatePath(Point start, Point goal) {
+        
+        return null;
+    }
+
+    /**
+     * Calculates the cost between 2 points using the Euclidean distance.
+     * @param start the base Point to calculate from
+     * @param goal the end Point of the calculation.
+     * @return returns the cost as a Double
+     */
+    public double heuristicCost(Point start, Point goal) {
+        int dx = start.x - goal.x;
+        int dy = start.y - goal.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    
+    /**
+     * Check whether a Point it's GameObject is a Collision or not.
+     * @param target the given Point to check.
+     * @return see description.
+     */
+    public boolean isCollision(Point target){
+        if(Bot.getBot().getWorldMap().get(target) instanceof Collision){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

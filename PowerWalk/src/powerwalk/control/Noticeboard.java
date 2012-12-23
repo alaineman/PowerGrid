c546b13cea666f1adacb77da1641585186882115
Vincent Wassenaar <belasting-dienst@live.nl>
2012-12-23 23:32:35 +0100

    Added Noticeboard.java

---
+++ PowerWalk/src/powerwalk/control/Noticeboard.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package powerwalk.control;
+
+import org.powerbot.game.api.methods.Widgets;
+import org.powerbot.game.api.wrappers.widget.WidgetChild;
+
+/**
+ *
+ * @author vync job
+ */
+public abstract class Noticeboard {
+
+    public static final Tab FEATURED = new Tab(1345, "Featured");
+    public static final Tab CHALLANGES = new Tab(1343, "Challanges");
+    public static final Tab QUESTS = new Tab(190, "Quests");
+    public static final Tab ACHIEVEMENTS = new Tab(917, "Achievements");
+    public static final Tab MINIGAMES = new Tab(1344, "Minigames");
+    /**
+     * Contains all values of the five tabpanels from the Noticeboard. Each of
+     * the values is described below;
+     *
+     * <ul> <li> Index 0, Value 190, Description: Quest tab. </li> <li> Index 1,
+     * Value 917, Description: Achievements tab. </li> <li> Index 2, Value 1343,
+     * Description: Challanges tab. </li> <li> Index 3, Value 1344, Description:
+     * Minigames tab. </li> <li> Index 4, Value 1345, Description: Featured tab.
+     * </li> </ul>
+     */
+    public static final Tab[] noticeboardTabs = {FEATURED, CHALLANGES, QUESTS, ACHIEVEMENTS, MINIGAMES};
+
+    /**
+     *
+     * @param tab
+     * @return
+     */
+    public static boolean openTab(Tab tab) {
+        if (tab == null) {
+            return false;
+        }
+        Tab current = getCurrentTab();
+        int tries = 5;
+        while(current == null && tries > 0) {
+            open();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+            }
+            current = getCurrentTab();
+            tries--;
+        }
+        WidgetChild[] search = Widgets.get(current.widgetNum).getChildren();
+        for (WidgetChild child : search) {
+            if (child.getText() != null && child.getText().contains(tab.name)) {
+                child.click(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static Tab getCurrentTab() {
+        for (Tab i : noticeboardTabs) {
+            if (Widgets.get(i.widgetNum).validate()) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Ensures that the Noticeboard is visible.
+     */
+    public static void open() {
+        WidgetManager.openNoticeboardTab();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+        }
+        for (Tab i : Noticeboard.noticeboardTabs) {
+            if (Widgets.get(i.widgetNum).validate()) {
+                return;
+            }
+        }
+        Widgets.get(1056, 60).click(true);
+    }
+
+    public static class Tab {
+
+        private int widgetNum;
+        private String name;
+
+        private Tab(int num, String name) {
+            widgetNum = num;
+            this.name = name;
+        }
+    }
+}
--- PowerWalk/src/powerwalk/control/WidgetManager.java
+++ PowerWalk/src/powerwalk/control/WidgetManager.java
@@ -4,16 +4,19 @@ import org.powerbot.game.api.methods.Widgets;
 import powerwalk.model.interact.Lodestone;
 
 /**
- * Manages opening of Widgets in order to access the various possibilities that 
+ * Manages opening of Widgets in order to access the various possibilities that
  * the RSBot environment provides.
+ *
  * @author Alaineman
  * @author P.Kramer
  */
 public abstract class WidgetManager {
-    
-    /** Contains all values of widgets that represent lodestone locations */
-    public final static int[] lodestoneValues = {7, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
 
+    /**
+     * Contains all values of widgets that represent lodestone locations
+     */
+    public final static int[] lodestoneValues = {7, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+    
     /**
      * Ensures that the Spellbook panel is visible
      */
@@ -21,6 +24,19 @@ public abstract class WidgetManager {
         if (!Widgets.get(275, 18).visible()) {
             Widgets.get(548, 123).click(true);
         }
+    }
+
+    /**
+     * Ensures the Noticeboard Tab is visible
+     */
+    public static void openNoticeboardTab() {
+        if (!Widgets.get(1056, 60).visible()) {
+            Widgets.get(548, 117).click(true);
+        }
+    }
+
+    public static void openQuestTab() {
+       ////
     }
 
     /**
@@ -68,13 +84,13 @@ public abstract class WidgetManager {
         } catch (InterruptedException ex) {
         }
         updateLodestones();
-    }    
-    
+    }
+
     private static void updateLodestones() {
-        for(int i : lodestoneValues){
-             if (Widgets.get(1092, i).getTextureId() < 10117) {
-                 Lodestone.addLodestone(i);
-             }
-        }        
+        for (int i : lodestoneValues) {
+            if (Widgets.get(1092, i).getTextureId() < 10117) {
+                Lodestone.addLodestone(i);
+            }
+        }
     }
 }

e0eef15b78cc20a32f6ea6c97483001fd7212df0
Vincent Wassenaar <belasting-dienst@live.nl>
2012-12-31 01:54:52 +0100

    Manifold update
    GnomeGlider and Minecart handle + waitFor done

--- PowerWalk/src/powerwalk/model/interact/handlers/manifold/GnomeGlider.java
+++ PowerWalk/src/powerwalk/model/interact/handlers/manifold/GnomeGlider.java
@@ -1,32 +1,54 @@
 package powerwalk.model.interact.handlers.manifold;
 
 import java.util.ArrayList;
+import org.powerbot.core.script.job.Task;
+import org.powerbot.game.api.methods.Widgets;
+import org.powerbot.game.api.methods.interactive.NPCs;
+import org.powerbot.game.api.util.Timer;
+import org.powerbot.game.api.wrappers.interactive.NPC;
 import powerwalk.model.Point;
 import powerwalk.model.interact.Transportable;
 
 /**
- * 
+ *
  * @author Alaineman
  */
-public class GnomeGlider extends Transportable{
-    
-    public GnomeGlider(Point p){
+public class GnomeGlider extends Transportable {
+
+    /**
+     * These are the gnome glider Person Id's.
+     * @see Person
+     */
+    public final static int[] values = {};
+
+    public GnomeGlider(Point p) {
         super(p.x, p.y, p.z, -1, new ArrayList<GnomeGlider>(7));
     }
 
     @Override
     protected void handle(Transportable dest) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        NPC gnome = NPCs.getNearest(values);
+        gnome.interact("Glider");
+        Timer wait = new Timer(5000);
+        while (wait.isRunning() && !Widgets.get(138).validate()) {
+            Task.sleep(50, 100);
+        }
+        if(dest.getDestinations().length > 1){
+        Widgets.get(138, dest.getWidgetNumber()).click(true);
+        }
+        waitForCompletion(dest);
     }
 
     @Override
     protected void waitForCompletion(Transportable dest) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Timer wait = new Timer(5000);
+        while (wait.isRunning() && Widgets.get(138).validate()) {
+            Task.sleep(50, 100);
+        }
     }
 
     @Override
     public boolean isAllowed(String method) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
 }
--- PowerWalk/src/powerwalk/model/interact/handlers/manifold/Minecart.java
+++ PowerWalk/src/powerwalk/model/interact/handlers/manifold/Minecart.java
@@ -1,52 +1,68 @@
 package powerwalk.model.interact.handlers.manifold;
 
 import java.util.Collection;
+import org.powerbot.core.script.job.Task;
+import org.powerbot.game.api.methods.Widgets;
+import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.SceneEntities;
+import org.powerbot.game.api.util.Timer;
 import org.powerbot.game.api.wrappers.node.SceneObject;
 import powerwalk.model.OutOfReachException;
 import powerwalk.model.Point;
 import powerwalk.model.interact.Transportable;
 
 /**
- * 
+ *
  * @author Alaineman
  */
 public class Minecart extends Transportable {
-    
-    public final static int[] values = { 28094, 7030, 7029, 7028 };
-        
-    public Minecart(Point p, Collection<? extends Minecart> dests){
-        super(p.x, p.y, p.z, -1, dests);        
+
+    /**
+     * These are the hatch and minecart object values.
+     */
+    public final static int[] values = {28094, 7030, 7029, 7028};
+
+    public Minecart(Point p, Collection<? extends Minecart> dests) {
+        super(p.x, p.y, p.z, -1, dests);
     }
-    
+
     @Override
     protected void handle(Transportable dest) throws OutOfReachException {
         Minecart current = (Minecart) dest;
-        if(dest.getDestinations().length > 1){
+        SceneObject cart = SceneEntities.getNearest(values);
+        if (cart == null || !cart.isOnScreen()) {
+            throw new OutOfReachException(dest.getPosition(), "No cart nearby.");
+        }
+        cart.click(true);
+        if (dest.getDestinations().length > 1) {
             int widgetCode = current.getWidgetNumber();
-            if(widgetCode != -1){
-                SceneObject cart = SceneEntities.getNearest(values);          
-                if(cart == null || !cart.isOnScreen()) {
-                    throw new OutOfReachException(dest.getPosition(), "No cart nearby.");
-                } 
-                cart.click(true); 
-                waitForCompletion(dest);
+            if (widgetCode != -1) {
+                Timer widgetTimer = new Timer(4000);
+                while (widgetTimer.isRunning() && !Widgets.get(889).validate()) {
+                    Task.sleep(15, 35);
+                }
+                Widgets.get(889, dest.getWidgetNumber()).click(true);
             } else {
                 throw new OutOfReachException(dest.getPosition(), "Has no widget value.");
             }
-        } else {
-            //toClient(dest);
         }
+        waitForCompletion(dest);
     }
 
     @Override
     protected void waitForCompletion(Transportable dest) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Timer riding = new Timer(6000);
+        while (riding.isRunning() && Players.getLocal().getAnimation() != 2148) {
+            Task.sleep(50, 100);
+        }
+        riding.reset();
+        while (riding.isRunning() && Players.getLocal().getAnimation() == 2148) {
+            Task.sleep(50, 100);
+        }
     }
 
     @Override
     public boolean isAllowed(String method) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-   
 }

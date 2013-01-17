6a929b64d2f3271720d5748e9302d761ffbc0cda
Patrick Kramer <ptrck.krmr@gmail.com>
2013-01-17 21:55:38 +0100

    Improved some parts of scriptLoader
    Can practically load any script, and keeps them loaded for over an hour
    at least (I tested this).

--- PowerWalk/src/ScriptLoader.java
+++ PowerWalk/src/ScriptLoader.java
@@ -2,11 +2,13 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import org.powerbot.core.bot.Bot;
@@ -16,9 +18,20 @@ import org.powerbot.core.script.Script;
 import org.powerbot.game.api.Manifest;
 import powerwalk.Starter;
 
+/**
+ * Loads a script and executes it on the RSBot client. Since a required class is 
+ * obfuscated, some reflection is done on the client.
+ * @author Chronio
+ */
 public class ScriptLoader {
     
     private Class<? extends ActiveScript> scriptClass = null;
+    
+    private static int stopped = 0;
+    private static int busy = 1;
+    private static int running = 2;
+    
+    private int state = stopped;
     
     public ScriptLoader(String scriptClassName) {
         try {
@@ -42,6 +55,7 @@ public class ScriptLoader {
     public void run() {
         // get scriptClass instance and run it.
         // at this moment, reflection has to be used to get to the ScriptLoader class
+        state = busy;
         Bot bot = Bot.getInstance();
         if (bot != null) {
             try {
@@ -61,11 +75,12 @@ public class ScriptLoader {
                 Constructor<?> cons = scriptDef.getDeclaredConstructor(Manifest.class);
                 Object scriptDefinition = cons.newInstance(scriptClass.getAnnotation(Manifest.class));
                 startMethod.invoke(handler, s, scriptDefinition);
-                System.out.println("[PowerWalk > ScriptLoader] Script launched");
+                System.err.println("[PowerWalk > ScriptLoader] Script launched");
+                state = running;
             } catch (NoSuchMethodException     | IllegalAccessException | IllegalArgumentException | 
                      InvocationTargetException | NoClassDefFoundError   | InstantiationException   |
                      SecurityException e) {
-                System.out.println("[PowerWalk] An error occurred while trying to load script:\n  " + e.getClass().getSimpleName() + ": " + e.getLocalizedMessage());
+                System.err.println("[PowerWalk] An error occurred while trying to load script:\n  " + e.getClass().getSimpleName() + ": " + e.getLocalizedMessage());
             }
         }
     }
@@ -101,7 +116,12 @@ public class ScriptLoader {
         JButton play = new JButton("Run " + getName());
         play.addActionListener(new ActionListener() {
             @Override public void actionPerformed(ActionEvent e) {
-                run();
+                if (state == stopped) {
+                    JButton theButton = (JButton)e.getSource();
+                    theButton.setText(getName() + " running");
+                    theButton.setEnabled(false);
+                    run();
+                }
             }
         });
         return play;
@@ -110,6 +130,18 @@ public class ScriptLoader {
     public static void main(String[] args) {
         Starter.main(new String[]{"-dev"});
         ScriptLoader l = new ScriptLoader("powerwalk.Starter");
-        
+        JButton b = l.createPlayButton();
+        b.setPreferredSize(new Dimension(200, 32));
+        JFrame lFrame = new JFrame("PW launcher");
+        try {
+            lFrame.setIconImage(ImageIO.read(ClassLoader.getSystemResource("icon_small.png")));
+        } catch (IOException e) {
+            System.err.println("[PowerWalk] failed to load imageicon due to " + e.getClass().getSimpleName());
+        }
+        lFrame.setResizable(false);
+        lFrame.setLayout(new BorderLayout(10, 5));
+        lFrame.add(b,"Center");
+        lFrame.pack();
+        lFrame.setVisible(true);
     }
 }

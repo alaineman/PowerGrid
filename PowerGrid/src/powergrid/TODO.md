035f3228436fcf2f8d9aef2794c01470ca364b47
Patrick Kramer <ptrck.krmr@gmail.com>
2013-04-03 21:50:37 +0200

    Implemented Player methods, but needs verification

--- PowerGrid/src/powergrid/TODO.md
+++ PowerGrid/src/powergrid/TODO.md
@@ -1,11 +1,10 @@
 ## TODO items ##
 
-- Place RSObjectDefLoaders in Mapper and pass along with RSGround objects.
-    -> Created DefinitionCache class for this purpose, which maintains the 
-       definitions for easy access.
 - Create Interactors and corresponding model classes
 - Create and hook listeners for RSMonitor
 - implement Player methods
+    -> @Alaineman: please check implemented methods and verify these are 
+                   correct. Some RSPlayer methods are unclear. -- Chronio
 - implement GraphNetwork methods
 - GUI for TaskManager and PG settings
 
--- PowerGrid/src/powergrid/model/world/Player.java
+++ PowerGrid/src/powergrid/model/world/Player.java
@@ -3,41 +3,66 @@ package powergrid.model.world;
 import org.powerbot.game.client.RSPlayer;
 
 /**
- *
- * @author patrick
+ * This class represents a Player in Runescape.
+ * @author Chronio
  */
 public class Player extends Mobile {
 
     private RSPlayer player;
     
+    /**
+     * Creates a new Player that represents the given RSPlayer
+     * @param player the RSPlayer this Player represents
+     */
     public Player(RSPlayer player) {
         super(player);
         this.player = player;
     }
+
+    @Override
+    public RSPlayer getRSCharacter() {
+        return player;
+    }
     
     @Override
     public int getID() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //XXX Chronio: "I'm not sure about this ID value, please verify"
+        return getRSCharacter().getComposite().getNPCID();
     }
 
     @Override
     public int getLevel() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return getRSCharacter().getLevel();
     }
 
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return getRSCharacter().getName();
     }
 
     @Override
     public String[] getActions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //XXX Chronio: "How can we find this out? It's not in the RSPlayerComposite"
+        return new String[0];
     }
 
     @Override
     public int getPrayerIcon() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return getRSCharacter().getPrayerIcon();
+    }
+    
+    /**
+     * @return an integer specifying the Player's Skull icon.
+     */
+    public int getSkullIcon() {
+        return getRSCharacter().getSkullIcon();
+    }
+    
+    /**
+     * @return an integer specifying the Player's Team.
+     */
+    public int getTeam() {
+        return getRSCharacter().getTeam();
     }
     
 }

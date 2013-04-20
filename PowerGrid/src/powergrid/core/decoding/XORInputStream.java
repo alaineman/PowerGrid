974dfa2e11b83c59f7ef01929d18909fa218e515
Vincent Wassenaar <V.Wassenaar@student.tudelft.nl>
2013-04-20 22:08:53 +0200

    XOR moved to C++ module
    also C++ settings on gitignore

--- PowerGrid/src/powergrid/core/decoding/XORInputStream.java
+++ PowerGrid/src/powergrid/core/decoding/XORInputStream.java
@@ -5,27 +5,43 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * @author Paris, Chronio
+ * @author Chronio, Alaineman
  */
 public class XORInputStream extends FilterInputStream {
 
-    private XOR xor;
+    private byte[] key;
+    private int opmode;
 
     public XORInputStream(InputStream in, byte[] key, int opmode) {
         super(in);
-        xor = new XOR(key, opmode);
+        this.key = key;
+        this.opmode = opmode;
     }
 
+    public int getOpmode() {
+        return opmode;
+    }
+
+    public byte[] getKey() {
+        return key;
+    }
+    
+    private native void rotate(byte[] b, int off, int l, byte[] key, int opmode);
+    
     @Override
     public synchronized int read() throws IOException {
-        return xor.rotateOne((byte) super.read()) & 0xff;
+        byte[] bs = new byte[1];
+        bs[0] = (byte) super.read();
+        rotate(bs, 0, 1, key, opmode);
+        return bs[0];
+        
     }
 
     @Override
     public synchronized int read(byte[] bytes, int off, int len)
             throws IOException {
         int result = super.read(bytes, off, len);
-        xor.rotate(bytes, off, len);
+        rotate(bytes, off, len, key, opmode);
         return result;
     }
 }

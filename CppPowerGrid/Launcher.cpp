315c38f7557b1cce8822b9dbade9c7b60b35e85f
Patrick Kramer <ptrck.krmr@gmail.com>
2013-04-22 00:20:30 +0200

    Cleaned up Launcher.cpp and added a bit of functionality for acquiring the Loader jar file.

--- CppPowerGrid/Launcher.cpp
+++ CppPowerGrid/Launcher.cpp
@@ -6,13 +6,11 @@
 #include <curlpp/Options.hpp>
 #include <curlpp/Exception.hpp>
 
+using namespace std;
+using curlpp::options::Url;
+using curlpp::options::WriteStream;
+
 namespace powergrid {
-    
-    // I'm too lazy to type <namespace>::<identifier> everytime.
-    using std::string;
-    using std::cout;
-    using std::endl;
-    using curlpp::options::Url;
     
     cURLpp::Easy easyHandle;
 
@@ -40,17 +38,29 @@ namespace powergrid {
             cout << "Could not find the required class on the classPath" << endl;
             return EXIT_FAILURE;
         }
-
+        
+        env->CallStaticVoidMethod(mainClass, "main", NULL);
+        
+        while (true /* running */) {
+            /* Read and update fields */
+        }
+        
+        jvm->DestroyJavaVM();
+        
         return EXIT_SUCCESS;
     }
 
     char* getRSLink() {
         char* link = NULL;
+        stringstream stream;
         try {
-            easyHandle.setOpt(new Url("http://www.runescape.com/k=3/l=en/jav_config.ws"));
+            easyHandle.setOpt(Url("http://www.runescape.com/k=3/l=en/jav_config.ws"));
+            easyHandle.setOpt(WriteStream(&stream))
+            easyHandle.perform();
         } catch (std::exception e) {
             cout << "Failed to set URL, exception occurred: " << e.what() << endl;
         }
+        //TODO dump and filter stream and store in link.
         return link;
     }
 

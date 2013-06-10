47c6c22fa66069b2aae534ff82ad65c095400d57
Vincent Wassenaar <V.Wassenaar@student.tudelft.nl>
2013-06-10 02:52:33 +0200

    BaseComponent update
    cache update - basecomponent made

--- src/Cachemodule/basecomponent.h
+++ src/Cachemodule/basecomponent.h
@@ -4,15 +4,18 @@
 #include <QMap>
 #include "widget.h"
 
+using namespace world;
 
 namespace cache{
 
-    class BaseComponent {
-      private:
-        //QMap<int, Widget*> set;
-
+    class BaseComponent : public QObject{
       public:
-
+        explicit BaseComponent(QObject* parent = NULL):QObject(parent){}
+        Widget getWidget(int n1, int n2 = -1, int n3 = -1, int n4 = -1);
+        bool containsWidget(int n1, int n2 = -1, int n3 = -1, int n4 = -1){return getWidget(n1,n2,n3,n4)!= NULL;}
+    public slots:
+        void clearWidget(Widget widget);
+        void clearAll();
     };
 }
 #endif

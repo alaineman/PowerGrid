7e73baf275f8b26df01c943dba10f1455d167f9d
Patrick Kramer <ptrck.krmr@gmail.com>
2014-02-23 18:23:25 +0100

    Finished Interface proxy

--- src/JACE/api/bridge/interface.cpp
+++ src/JACE/api/bridge/interface.cpp
@@ -1,5 +1,5 @@
 #include "interface.h"
-//#include "interfacechild.h"
+#include "interfacechild.h"
 
 namespace api {
 namespace bridge {
@@ -7,7 +7,7 @@ namespace bridge {
 IMPL_JACE_CONSTRUCTORS(Interface)
 IMPL_RSCLASS_GET(Interface)
 
-// IMPL_ARRAY_METHOD(Interface, getChildren, InterfaceChild)
+IMPL_ARRAY_METHOD(Interface, getChildren, InterfaceChild)
 
 } // namespace bridge
 } // namespace api

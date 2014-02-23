dfdf289d1fd27a3653a252af742fea3023d207dc
Patrick Kramer <ptrck.krmr@gmail.com>
2014-02-23 19:45:13 +0100

    Fixed typo in GroundBytes proxy.

--- src/JACE/api/bridge/groundbytes.cpp
+++ src/JACE/api/bridge/groundbytes.cpp
@@ -6,7 +6,7 @@ namespace bridge {
 IMPL_JACE_CONSTRUCTORS(GroundBytes)
 IMPL_RSCLASS_GET(GroundBytes)
 
-IMPL_ARRAY3_METHOD(GroundBytes, gteBytes, JByte)
+IMPL_ARRAY3_METHOD(GroundBytes, getBytes, JByte)
 
 } // namespace bridge
 } // namespace api

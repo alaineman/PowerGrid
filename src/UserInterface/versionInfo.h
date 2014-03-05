bfa028a5e0277f54c68c480fb4ba837d0a4a5616
Patrick Kramer <ptrck.krmr@gmail.com>
2014-03-05 20:06:18 +0100

    Fixed issue in versionInfo.h when read from RC file.

--- src/UserInterface/versionInfo.h
+++ src/UserInterface/versionInfo.h
@@ -19,10 +19,8 @@
 #ifndef VERSIONINFO_H
 #define VERSIONINFO_H
 
-#include <QString>
-
 /// The name of PowerGrid as a QString literal
-#define PG_NAME_STR QStringLiteral("PowerGrid")
+#define PG_NAME_STR "PowerGrid"
 
 
 /// The version of Powergrid as a decimal number

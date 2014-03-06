aa3933f1ffe8207dea8ec9e52c74794ce9f2c755
Patrick Kramer <ptrck.krmr@gmail.com>
2014-03-06 19:10:45 +0100

    Completed api::component::Interaction class.

--- src/JACE/api/component/interaction.cpp
+++ src/JACE/api/component/interaction.cpp
@@ -7,5 +7,17 @@ using java::lang::Object;
 namespace api {
 namespace component {
 
+Interaction::Interaction(QMap<QString, Action *> _actions, java::lang::Object ref, QObject *parent) :
+        Component(ref, parent), actions(_actions) {
+}
+
+Action* Interaction::getAction(QString name) const {
+    return actions.value(name, Q_NULLPTR);
+}
+
+QMap<QString, Action*> Interaction::getActionMap() const {
+    return actions;
+}
+
 } // namespace component
 } // namespace api
--- src/JACE/api/component/interaction.h
+++ src/JACE/api/component/interaction.h
@@ -10,13 +10,47 @@ using jace::JField;
 namespace api {
 namespace component {
 
-//class Action; //need to make action structure
+using java::lang::Object;
+class Action;
 
+/**
+ * \brief Adds interactions to an Entity.
+ *
+ * This Component type can be used to assign actions to Entities in the
+ * Runescape world. These actions can relate to actual actions available
+ * in the Runescape client, or actions added by PowerGrid (or any of its Plugins).
+ */
 class Interaction : public Component {
     Q_OBJECT
 private:
+    QMap<QString, Action*> actions;
 public:
-//    explicit ID(Object ref, QList<JField<Action>> id, QObject* parent = 0); //List of Action ??
+    /**
+     * @brief Creates a new Interaction Component
+     * @param _actions the actions that can be performed
+     * @param ref [optional] the reference Object
+     * @param parent [optional] the parent of this Component
+     */
+    explicit Interaction(QMap<QString, Action*> _actions, Object ref = Object(), QObject* parent = 0);
+
+    /**
+     * @brief Returns the Action* with the given name.
+     *
+     * If no Action exists with that name, a NULL pointer is
+     * returned.
+     * @param name the name of the requested Action
+     * @return the Action, or NULL if it doesn't exist.
+     */
+    Action* getAction(QString name) const;
+    /**
+     * @brief Returns a QMap with all available Actions
+     *
+     * The returned QMap maps the name of the Action to the
+     * Action*.
+     * @return a QMap with the available Actions.
+     */
+    QMap<QString, Action*> getActionMap() const;
+
 };
 } // namespace component
 } // namespace api
